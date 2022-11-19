9f9727530f94d4d9209dadf6c46795d169ec7846
Shrestha Tripathi <shrestha.tripathi@example.com>
2022-11-19 14:43:35 +0530

    Initial Project Setup

---
+++ packages/oitrender/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/oitrender/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div>Test</div>;
+}
+
+export default Navbar;
