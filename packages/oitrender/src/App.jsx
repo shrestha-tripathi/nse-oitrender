44c69ed58a15ad6d991c463117c86af9b7798082
Shrestha Tripathi <shrestha.tripathi@example.com>
2022-12-03 17:19:17 +0530

    added navbar with react-router

--- packages/oitrender/src/App.jsx
+++ packages/oitrender/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import About from "./components/Pages/About";
